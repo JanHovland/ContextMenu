cb40c2c120bae284ad3d04b96eb68442e811902d
Jan Hovland <jan.hovland@lyse.net>
2020-09-23 12:35:24 +0200

    Update

--- ContextMenu/ContentView.swift
+++ ContextMenu/ContentView.swift
@@ -5,14 +5,35 @@
 //  Created by Jan Hovland on 23/09/2020.
 //
 
+/// Dette er oppbygging av .contextMenu i iOS 14
+
 import SwiftUI
 
+let menuItems = ContextMenu {
+    Button("♥️ - Hearts", action: selectHearts)
+    Button("♣️ - Clubs", action: selectClubs)
+    Button("♠️ - Spades", action: selectSpades)
+    Button("♦️ - Diamonds", action: selectDiamonds)
+}
+
 struct ContentView: View {
+    private var shouldShowMenu = true
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("Favorite Card Suit")
+                    .padding()
+                    .contextMenu(shouldShowMenu ? menuItems : nil)
+            }
+        }
     }
 }
+
+func selectHearts() {}
+func selectClubs() {}
+func selectSpades() {}
+func selectDiamonds() {}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
