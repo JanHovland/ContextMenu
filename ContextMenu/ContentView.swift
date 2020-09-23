2f61b105c8aba838a5832dc916d40a98079aaf76
Jan Hovland <jan.hovland@lyse.net>
2020-09-23 12:30:25 +0200

    Initial Commit

---
+++ ContextMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContextMenu
+//
+//  Created by Jan Hovland on 23/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ContextMenu/ContextMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuApp.swift
+//  ContextMenu
+//
+//  Created by Jan Hovland on 23/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
